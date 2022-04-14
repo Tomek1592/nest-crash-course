c0e3a05d95d789a6599fc5cde44f6a51b22f0210
Tomasz Wołoszyk <Tomek1592@gmail.com>
2022-04-14 18:09:54 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const prismaService = app.get(PrismaService);
 
   await app.listen(process.env.PORT || 3000);
